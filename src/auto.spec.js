9c815fce024fa1c68387469296b4abd91ed463ce
wilnermena <wi_an9@hotmail.com>
2023-09-16 11:06:53 -0400

    Primer Tdd

--- src/auto.spec.js
+++ src/auto.spec.js
@@ -1,4 +1,19 @@
 import sumar from "./sumador.js";
 
+describe("Auto gira a la izquierda",()=>{
+  it("Gira a la Izquierda desde el norte",()=>{
+    let auto = new Auto(0, 0, "N");
+    auto.ejecutar("I");
+    expect(auto.posicionActual()).toEqual("0,0O");
+  });
 
+})
 
+class Auto{
+  ejecutar(){
+
+  }
+  posicionActual(){
+    return "0,0O"
+  }
+}
